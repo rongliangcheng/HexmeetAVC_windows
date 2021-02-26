8f8aa4b20a35b271728d285dd03d57adc52f72b7
rongliang <cheng_rongliang@163.com>
2021-02-26 09:35:05 +0800

    延长等待时间

--- test/alogin/test_login.py
+++ test/alogin/test_login.py
@@ -60,17 +60,17 @@ def test_login_with_wrong_server_address():
     user_login.login_fail_commit()
 
 
-# @pytest.mark.flaky(rerun=1, rerun_delay=2)
-# @allure.parent_suite("登录")
-# @allure.suite("尝试5遍被锁5分钟")
-# @allure.feature("测试HEXMEET APP的登录界面的操作")
-# @allure.story("尝试5遍被锁5分钟")
-# def test_login_with_5_times_wrong_password():
-#     for i in range(6):
-#         __user_login("172.25.0.9", "hexautotest6", "12346")
-#         sleep(1)
-#         user_login.login_fail_commit()
-#         sleep(10)
+@pytest.mark.flaky(rerun=1, rerun_delay=2)
+@allure.parent_suite("登录")
+@allure.suite("尝试5遍被锁5分钟")
+@allure.feature("测试HEXMEET APP的登录界面的操作")
+@allure.story("尝试5遍被锁5分钟")
+def test_login_with_5_times_wrong_password():
+    for i in range(6):
+        __user_login("172.25.0.9", "hexautotest6", "12346")
+        sleep(1)
+        user_login.login_fail_commit()
+        sleep(10)
 
 
 @pytest.mark.flaky(rerun=1, rerun_delay=2)
@@ -79,7 +79,7 @@ def test_login_with_wrong_server_address():
 @allure.feature("测试HEXMEET APP的登录界面的操作")
 @allure.story("正常登录成功")
 def test_login_with_normal():
-    # sleep(300)
+    sleep(300)
     __user_login("172.25.0.9", "hexautotest6", "123456")
 
 
